e27025833c0905819e7f12829bd1661de195304f
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-11-08 17:40:12 -0500

    Generates README

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
 const questions = [
-
-];
+{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "What is the description of your project"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project?"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "What are the instructions for using your project?"
+},
+{
+    type: "input",
+    name: "license",
+    message: "Enter in your license information for your project."
+},
+{
+    type: "input",
+    name: "contributing",
+    message: "What are the contributing guidelines for your project?",
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Enter in your tests"
+},
+{
+    type: "input",
+    name: "github",
+    message: "What is your GitHub user name?"
+},
+{
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+}];
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -9,8 +58,19 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    return inquirer.prompt(questions);
 }
 
 // function call to initialize program
-init();
+init()
+    .then((answers) => {
+        console.log(answers);
+        const markdown = generateMarkdown(answers);
+        return writeFileAsync("README.md", markdown);
+    })
+    .then(function() {
+        console.log("Successfully wrote README file");
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,22 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `#${data.title}
+  ###Description
+  ${data.description}\n
+  ###Installation
+  ${data.installation}\n
+  ###Usage
+  ${data.usage}\n
+  ###License
+  ${data.lisence}\n
+  ###Contributing
+  ${data.contributing}\n
+  ###Tests
+  ${data.tests}\n
+  ###Questions
+  For questions contact me at:
+  Email: ${data.email}
+  [github.com/${data.github}]
 `;
 }
 
